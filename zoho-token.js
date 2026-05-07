d6d01dc2fd7eedc2616c8e65b0dc87c838b32144
jakunatravel <info@jakunatravel.com>
2026-05-07 16:26:30 +0200

    Create zoho-token.js

---
+++ zoho-token.js
@@ -0,0 +1,38 @@
+exports.handler = async (event) => {
+  const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers: corsHeaders, body: '' };
+  }
+
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, headers: corsHeaders, body: 'Method Not Allowed' };
+  }
+
+  const params = new URLSearchParams(JSON.parse(event.body));
+  const base = 'https://accounts.zoho.eu';
+
+  try {
+    const response = await fetch(`${base}/oauth/v2/token`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: params.toString()
+    });
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: corsHeaders,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
